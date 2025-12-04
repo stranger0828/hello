e5709d40054c50644b828dbaa5ac4c1c1caadf0f
stranger0828 <zhuyulu_0828@163.com>
2025-12-04 10:48:00 +0800

    新增模块化使用，稍微了解一下包的调用...

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"hello/hello/modules/print"
 )
 
 func main() {
 	fmt.Println("hello world!")
+	print.Printdetail()
 }
---
+++ modules/print/print.go
@@ -0,0 +1,7 @@
+package print
+
+import "fmt"
+
+func Printdetail() {
+	fmt.Println("who is hero")
+}
